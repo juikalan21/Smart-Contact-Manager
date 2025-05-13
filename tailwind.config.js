bc9126e7def3d8b10db4fb8ca59622f85a8a7c26
juikalan <juikalan21@gmail.com>
2025-05-13 22:59:20 +0530

    tailwind css setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/main/resources/**/*.{html,js}"],
+  content: ["./src/main/resources/**/*.{html,js}"], 
   theme: {
     extend: {},
   },
